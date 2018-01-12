88d8596650797692dfdb8ef0f8613c79a6d36255
oscardagrach <ryan@edited.us>
2018-01-12 14:34:26 -0600

    tegra: cpuidle: check for t11x

--- include/linux/tegra-cpuidle.h
+++ include/linux/tegra-cpuidle.h
@@ -109,7 +109,7 @@ void tegra_pd_update_target_residency(struct cpuidle_state *state);
 #endif /* CONFIG_PM_SLEEP */
 
 #if defined(CONFIG_CPU_IDLE) && defined(CONFIG_PM_SLEEP) \
-	&& defined(CONFIG_ARCH_TEGRA_12x_SOC)
+	&& defined(CONFIG_ARCH_TEGRA_12x_SOC) || defined(CONFIG_ARCH_TEGRA_11x_SOC)
 void tegra_pd_in_idle(bool enable);
 #else
 static inline void tegra_pd_in_idle(bool enable) {}
